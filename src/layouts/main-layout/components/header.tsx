a70ef6bcf2711f54722242a4b971af94074300f9
Buza Dan <buzadandeveloper@gmail.com>
2026-07-04 21:28:10 +0300

    refactor: adjust header title

--- src/layouts/main-layout/components/header.tsx
+++ src/layouts/main-layout/components/header.tsx
@@ -6,7 +6,7 @@ export const Header = () => {
   return (
     <header className="navbar bg-base-100 border-b border-base-300 px-4 shadow-sm">
       <div className="flex-1">
-        <span className="text-xl font-bold">Pokémon Games</span>
+        <span className="text-xl font-bold">Poké Games</span>
       </div>
       <nav className="flex gap-2">
         {routes.map((route, index) => (
